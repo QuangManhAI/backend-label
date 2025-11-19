c391bc26eaf634717ddad978dbb49df11a522b2c
QuangManhAI <py.quangmanh.ai@gmail.com>
2025-11-19 20:29:52 +0700

    railway to deploy

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin:"*",
   });
 
-app.useStaticAssets("/home/quangmanh/Documents/pineline/back-end-label/uploads", {
-  prefix: "/uploads",
-});
-
   await app.listen(process.env.PORT || 3001);
-  console.log(`Server running at http://localhost:${process.env.PORT || 3001}`);
+  console.log(`Server running at port ${process.env.PORT || 3001}`);
 }
 bootstrap();
